2261727add80304ca4617334bc94c0b5091db6a0
LipengNing <lning@bwh.harvard.edu>
2018-04-12 10:36:08 -0400

    Update icosahedrom.m vec.m.

--- icosahedron.m
+++ icosahedron.m
@@ -1,19 +1,16 @@
-function [u,fcs] = icosahedron(L)
-
-% ICOSAHEDRON Icosahedral sampling the unit sphere.  
-%   ICOSAHEDRON(L) samples the unit sphere using an L-order tesselation of the icosa-
-% hedron. If L = 0, the coordinates of the non-tesselated icosahedron are returned.  
+function [u,fcs] = icosahedron(level)
+% ICOSAHEDRON Sampling the unit sphere  
+%  ICOSAHEDRON(level) samples the unit sphere using a regular icosahedron
+% tessellation of the order defined by 'level'.
 %
-%                                [u,fcs] = icosahedron(L)
-%   Input:
-%                 L - tessellation order
-%   Output:
+%                     [u,fcs] = icosahedron(level)
+% Input:
+%             level - tessellation order
+% Output:
 %                 u - N-by-3 matrix of samples of the unit sphere
 %               fcs - vertices-to-faces correspondence matrix
 %
-% See also SPIRALSAMPLE
-%
-% written by Oleg Michailovich, 2009/05/02
+% written by Oleg Michailovich, February 5th, 2009
 
 C=1/sqrt(1.25);
 t=(2*pi/5)*(0:4)';
@@ -21,8 +18,8 @@ u1=C*[cos(t) sin(t) 0.5*ones(5,1)];
 u2=C*[cos(t+0.2*pi) sin(t+0.2*pi) -0.5*ones(5,1)];
 u=[[0 0 1]; u1; u2; [0 0 -1]];
 
-if (L>0),
-    for lev=1:L,
+if (level>0),
+    for lev=1:level,
         fcs=convhulln(u);
         N=size(fcs,1);
         U=zeros(3*N,3);
@@ -36,11 +33,11 @@ if (L>0),
         U=U./repmat(sqrt(sum(U.^2,2)),[1 3]);
         u=[u; U]; %#ok<AGROW>
     end    
-    [dumy,ind]=sort(u(:,3),1,'descend');
+    [C,ind]=sort(u(:,3),1,'descend');
     u=u(ind,:);
     index=find(u(:,3)==0);
     v=u(index,:);
-    [dumy,ind]=sort(v(:,2),1,'descend');
+    [C,ind]=sort(v(:,2),1,'descend');
     u(index,:)=v(ind,:);
 end
 
